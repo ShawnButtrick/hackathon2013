848914ab3d9045d5e667605aa229c055435ea174
Shawn Buttrick <shawn.buttrick@pearson.com>
2013-06-06 10:24:15 -0400

    Butchered the Subscribe Controller. PS its Tonys fault.

--- src/main/java/com/pearson/ed/prospero/forms/SubscribeController.java
+++ src/main/java/com/pearson/ed/prospero/forms/SubscribeController.java
@@ -1,26 +1,17 @@
 package com.pearson.ed.prospero.forms;
 
-import java.util.Date;
-
-import javax.crypto.spec.PSource;
 import javax.validation.Valid;
 
 import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpConnectionManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import com.pearson.ed.eventing.impl.prospero.publisher.ProsperoHttpClient;
-import com.pearson.ed.eventing.impl.prospero.subscriber.ProsperoHttpClientSubscriber;
-import com.pearson.ed.prospero.lookups.EnviroLookup;
 
 @Controller
 @RequestMapping("/subscribeform")
@@ -30,19 +21,11 @@ public class SubscribeController
 	
 	//ProsperoHttpClientSubscriber prosperoHttpClient;
 	private HttpClient httpClient;
-	private EnviroLookup enviroLookup;
 
 	
 	public HttpClient getHttpClient() { return httpClient; }
 	public void setHttpClient(HttpClient httpClient) { this.httpClient = httpClient; }
 
-	public EnviroLookup getEnviroLookup() { return enviroLookup; }
-	public void setEnviroLookup(EnviroLookup enviroLookup) { this.enviroLookup = enviroLookup; }
-	
-
-	
-	
-	
 	// Invoked initially to create the "form" attribute
 	// Once created the "form" attribute comes from the HTTP session (see @SessionAttributes)
 	@ModelAttribute("subscribeBean")
@@ -50,14 +33,7 @@ public class SubscribeController
 	{
 		SubscribeBean sb = new SubscribeBean();
 		
-		sb.setClientId("SMS-CLIENT-ID");
-		sb.setClientString("SMS-CLIENT-STRING");
-		
-		sb.setSystem("SMS");
-		sb.setSubSystem("SMS");
-		
-		sb.setMessageType("SmsUserEvent");
-		sb.setTags("SMS:SmsUserEvent");
+		sb.setSearchString("SMS-CLIENT-STRING");
 		
 		return sb;
 	}
@@ -81,25 +57,10 @@ public class SubscribeController
 		String message; // = "Status:Form submitted successfully.  Bound " + formBean;
 		String error;
 		
-		String url = enviroLookup.getEnviroUrl(formBean.getEnviro()) + enviroLookup.getEnviroSubscribePath(formBean.getEnviro()) ;
-		
-		ProsperoHttpClientSubscriber prosperoHttpClient = new ProsperoHttpClientSubscriber(httpClient, httpClient.getHttpConnectionManager(), url);
-		
-		prosperoHttpClient.setServerUrlString(enviroLookup.getEnviroUrl(formBean.getEnviro().toString()) + "/v1/subscription");
 		try
 		{
-			message = "Result: " + prosperoHttpClient.subscribe(
-				new Date(), 
-				formBean.getPrincipal(), 
-				formBean.getKey(), 
-				formBean.getCallbackUrl(), 
-				formBean.getMessageType(), 
-				formBean.getClientId(), 
-				formBean.getClientString(), 
-				formBean.getSystem(), 
-				formBean.getSubSystem(), 
-				formBean.getTags()				
-				);
+			message = "Results: ";
+			//DO SHIT HERE
 		}
 		catch(Exception e)
 		{
